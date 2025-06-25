6632814ed034583197b765068a2bc3b7ff79784c
MateoM376 <mateodematteis010609@gmail.com>
2025-06-25 10:08:15 +0200

    first commit

---
+++ test.js
@@ -0,0 +1,34 @@
+function cacherpartie1(){
+let partie2 = document.getElementById("partie1")
+.style.display = "none";
+}
+function cacherpartie2(){
+let partie2 = document.getElementById("partie2")
+.style.display = "none";
+}
+function cacherpartie3(){
+let partie2 = document.getElementById("partie3")
+.style.display = "none";
+}
+function cacherpartie4(){
+let partie2 = document.getElementById("partie4")
+.style.display = "none";
+}
+function montrerpartie1(){
+   document.getElementById("partie1").style.display = "block";
+}
+function montrerpartie2(){
+   document.getElementById("partie2").style.display = "block";
+}
+function montrerpartie3(){
+   document.getElementById("partie3").style.display = "block";
+}
+function montrerpartie4(){
+   document.getElementById("partie4").style.display = "block";
+}
+document.querySelectorAll('.cv-left button').forEach(btn => {
+    btn.addEventListener('click', function() {
+        document.querySelectorAll('.cv-left button').forEach(b => b.classList.remove('selected'));
+        this.classList.add('selected');
+    });
+});
